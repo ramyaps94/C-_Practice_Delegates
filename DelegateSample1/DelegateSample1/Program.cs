69b1110d455365a541299ba0a6b22794847d7186
Ramya P S <ramya.sudarshan1994@gmail.com>
2017-03-07 20:03:43 +0530

    Sample Program to demonstarate Delegates

---
+++ DelegateSample1/DelegateSample1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateSample1
+{
+    class DelegateSample
+    {
+        public delegate void printGreetingMsgDelegate(string s);
+        public static void Main(string[] args)
+        {
+            printGreetingMsgDelegate del = new printGreetingMsgDelegate(PrintGreetingMessage);
+            del("Ramya");
+            Console.ReadLine();
+
+        }
+
+        public static void PrintGreetingMessage(string name)
+        {
+            Console.WriteLine("Hai : {0} !!! Welcome to programming world :)",name);
+        }
+    }
+}
